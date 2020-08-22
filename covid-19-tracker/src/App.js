3111195570830aa7fe3684cc9e03858c19eb5898
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-08-23 03:06:50 +0530

    add all countries api

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -1,14 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import {
-  FormControl,
+  FormControl, Select, MenuItem
 } from "@material-ui/core";
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getContriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getContriesData();
+
+  }, []);
+
   return (
     <div className="App">
-      <h1>COVID-19-TRACKER</h1>
-      <FormControl className="app_dropdown">
-      </FormControl>
+      <div className="app_header">
+        <h1>COVID-19-TRACKER</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+
+            {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide</MenuItem> */}
+          </Select>
+        </FormControl>
+
+      </div>
     </div>
   );
 }
