841945e999cdbac519dc7ddf95c2fd128c45d045
SuraMahesh <surangamunasinghe22568@gmail.com>
2020-08-25 03:43:49 +0530

    added table.js4

--- covid-19-tracker/src/App.js
+++ covid-19-tracker/src/App.js
@@ -4,11 +4,21 @@ import {
   FormControl, Select, MenuItem, Card, CardContent
 } from "@material-ui/core";
 import InfoBox from './infoBox';
+import Table from './Table';
 import Map from './Map';
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState(['worldwide']);
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setCountryInfo(data);
+      });
+  }, []);
 
   useEffect(() => {
     const getContriesData = async () => {
@@ -19,7 +29,7 @@ function App() {
             name: country.country,
             value: country.countryInfo.iso2,
           }));
-
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -78,6 +88,8 @@ function App() {
       <Card className="app__right">
         <CardContent>
             <h3>Live Cases</h3>
+            <Table countries={tableData}>
+            </Table>
             <h3>Worldwide new cases</h3>
         </CardContent>
 
